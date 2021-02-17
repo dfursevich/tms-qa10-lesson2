c5964a0406f055d0a1d160fc9aa12f60530626a6
Dzmitry Fursevich <d.fursevich@severex.io>
2021-02-17 21:48:35 +0300

    print

---
+++ src/Run8.java
@@ -0,0 +1,12 @@
+public class Run8 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("World");
+
+        System.out.print("Hello1");
+        System.out.print("World1");
+
+        System.out.println("Hello3");
+        System.out.print("Hello3\n");
+    }
+}
