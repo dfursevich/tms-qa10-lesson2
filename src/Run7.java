a7904954e904f2cc5a5178bbcf1b99b85a705479
Dzmitry Fursevich <d.fursevich@severex.io>
2021-02-17 21:03:23 +0300

    for

---
+++ src/Run7.java
@@ -0,0 +1,15 @@
+public class Run7 {
+    public static void main(String[] args) {
+        double money = 100;
+        for (int i = 1; i <= 15; i++) {
+            if (i % 5 == 0) {
+                money =  money * 1.16;
+            } else {
+                money =  money * 1.15;
+            }
+            System.out.println("money=" + money + ", year=" + i);
+        }
+
+        System.out.println(money);
+    }
+}
